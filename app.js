19d56ebd166fc51841736319c1e6a1a121fb65fc
AlejandroMoreno <alemoga9@hotmail.com>
2020-10-19 21:23:24 +0100

    Nueva ruta

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Ruta servicios
+var serviciosRouter = require('./routes/servicios');
+
+//
+
 var app = express();
 
 // view engine setup
@@ -21,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//
+app.use('/servicios', serviciosRouter);
+//
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
